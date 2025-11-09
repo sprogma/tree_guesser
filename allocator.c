a8ab1af670a65089b6bc1bbaedaa184a5097c1e1
sprogma <mux-228@yandex.ru>
2025-11-09 19:25:40 +0300

    object structure + begin of node allocator

---
+++ allocator.c
@@ -0,0 +1,203 @@
+#include "tree.h"
+
+struct node_allocator *allocator_create()
+{
+    struct node_allocator *allocator = calloc(1, sizeof(*allocator));
+    
+    allocator->nodes_alloc = 1024;
+    allocator->nodes = calloc(1, sizeof(*allocator->nodes) * allocator->nodes_alloc);
+    allocator->lock = SRWLOCK_INIT;
+    allocator->loaded_nodes = 0;
+    
+    AcquireSRWLockExclusive(&allocator->lock);
+    
+    /* start optimization process */
+    // TODO:
+    
+    ReleaseSRWLockExclusive(&allocator->lock);
+    
+    return allocator;
+}
+
+
+void allocator_free(struct node_allocator *allocator)
+{
+    /* lock */
+    AcquireSRWLockExclusive(&allocator->lock);
+    /* stop all other processes? */
+    // TODO:
+    
+    
+    /* free allocator */
+    for (int i = 0; i < allocator->nodes_len; ++i)
+    {
+        if (res == UNLOADED_NODE)
+        {
+            fprintf(stderr, "Not implemented: unloading nodes\n");
+            *(int *)NULL = 1;
+            // TODO:
+        }
+        else
+        {
+            free(allocator->nodes[i]);
+        }
+    }
+    free(allocator);
+    
+    // no release, becouse alloactor was deleted.
+    // ReleaseSRWLockExclusive(&allocator->lock);
+}
+
+
+void allocator_initializate_node(struct node_allocator *allocator, int64_t node_id)
+{
+    /* !!! at begining of this function, allocator->lock must be 
+           locked with exclusive access !!! */
+}
+
+
+int64_t allocator_create_node(struct node_allocator *allocator)
+{
+    AcquireSRWLockShared(&allocator->lock);
+    /* allocate new node */
+    for (int i = 0; i < allocator->nodes_alloc; ++i)
+    {
+        if (allocator->nodes[i] == NULL)
+        {
+            /* try to insert node */
+            /* change access on exclusive */
+            ReleaseSRWLockShared(&allocator->lock);
+            AcquireSRWLockExclusive(&allocator->lock);
+            
+            /* node could be allocated while changing access,
+               so check that it is still free */
+            if (allocator->nodes[i] == NULL)
+            {
+                allocator_initializate_node(allocator, i);
+                
+                ReleaseSRWLockExclusive(&allocator->lock);
+                return i;
+            }
+            
+            /* else - continue searching */
+            ReleaseSRWLockExclusive(&allocator->lock);
+            AcquireSRWLockShared(&allocator->lock);
+        }
+    }
+    ReleaseSRWLockShared(&allocator->lock);
+    /* here - almost all table was used (may be not all)
+       so reallocate node array */
+    AcquireSRWLockExclusive(&allocator->lock);
+    
+    /* realloc */
+    int64_t new_id = allocator->nodes_alloc;
+    
+    allocator->nodes_alloc = 2 * allocator->nodes_alloc + !allocator->nodes_alloc;
+    void *new_ptr = realloc(allocator->nodes, sizeof(*allocator->nodes) * allocator->nodes_alloc);
+    if (new_ptr == NULL)
+    {
+        /* can't insert new node */
+        ReleaseSRWLockExclusive(&allocator->lock);
+        return INVALID_NODE_ID;
+    }
+    
+    /* now, state is exclusive, so use it to insert new node */
+    allocator_initializate_node(allocator, new_id);
+    
+    ReleaseSRWLockExclusive(&allocator->lock);
+}
+
+
+void allocator_load_node(struct node_allocator *allocator, int64_t node_id)
+{
+    fprintf(stderr, "Not implemented: loading/unloading nodes\n");
+    *(int *)NULL = 1;
+    // TODO:
+}
+
+
+void allocator_unload_node(struct node_allocator *allocator, int64_t node_id)
+{
+    fprintf(stderr, "Not implemented: loading/unloading nodes\n");
+    *(int *)NULL = 1;
+    // TODO:
+}
+
+
+struct node *allocator_acquire_node(struct node_allocator *allocator, int64_t node_id, int32_t exclusive)
+{
+    AcquireSRWLockShared(&allocator->lock);
+    
+    /* if node was unloaded */
+    struct node *res = &allocator->nodes[node_id];
+    if (res == NULL)
+    {
+        /* not allocated node */
+        return NULL;
+    }
+    else if (res == UNLOADED_NODE)
+    {
+        /* release allocator, load node, and acquire it back
+           (becouse load_node will acquire exclusive access) */
+        ReleaseSRWLockShared(&allocator->lock);
+        
+        /* load_node also will acquire node, becouse in another
+           way, node can be unloaded before next lock. */
+        allocator_load_node(allocator, node_id, exclusive);
+        
+        AcquireSRWLockShared(&allocator->lock);
+        
+        
+        /* check - is node loaded or freed? 
+           (if node was freed before loading) */
+        res = &allocator->nodes[node_id];
+        assert(res != UNLOADED_NODE);
+        if (res == NULL)
+        {
+            return NULL;
+        }
+        /* - node is loaded */
+    }
+    else
+    {
+        /* need to acquire node */
+        if (exclusive)
+        {
+            AcquireSRWLockExclusive(&res->lock);
+        }
+        else
+        {
+            AcquireSRWLockShared(&res->lock);
+        }
+    }
+    /* update last access token */
+    int64_t access_token = allocator->access_token++;
+    res->last_access = access_token;
+        
+    ReleaseSRWLockShared(&allocator->lock);
+    
+    return res;
+}
+
+void allocator_release_node(struct node_allocator *allocator, int64_t node_id, int32_t exclusive)
+{
+    AcquireSRWLockShared(&allocator->lock);
+    
+    /* release node */
+    struct node *res = &allocator->nodes[node_id];
+    assert(res != NULL);
+    assert(res != UNLOADED_NODE);
+    
+    if (exclusive)
+    {
+        ReleaseSRWLockExclusive(&res->lock);
+    }
+    else
+    {
+        ReleaseSRWLockShared(&res->lock);
+    }
+        
+    ReleaseSRWLockShared(&allocator->lock);
+    
+    return res;
+}
---
+++ tree.h
@@ -0,0 +1,118 @@
+#ifndef TREE_H
+#define TREE_H
+
+#include "inttypes.h"
+#include "windows.h"
+
+struct quesion
+{
+    char *text;
+};
+
+
+struct record
+{
+    char *name;
+};
+
+
+enum node_type
+{
+    NODE_VARIANT,
+    NODE_LEAF,
+};
+
+struct node
+{
+    enum node_type type;
+    _Atomic int32_t ptr_count;
+    _Atomic int64_t last_access;
+    SRWLOCK lock;
+};
+
+
+struct node_variant
+{
+    struct node;
+    int64_t l, r;
+    struct question *quesion;
+};
+
+
+struct node_leaf
+{
+    struct node;
+    struct record *guess;    
+};
+
+
+struct tree_version
+{
+    struct node *root;
+    int64_t size;
+    int64_t parent;
+    /* for searching LCA */
+    int64_t far_parent;
+    int64_t depth;
+};
+
+
+struct tree
+{
+    struct tree_version *versions;
+    int64_t              versions_len;
+    int64_t              versions_alloc;
+    
+    SRWLOCK versions_lock;
+
+    struct node_allocator *allocator;
+};
+
+
+struct akinator
+{
+    struct tree *tree;
+};
+
+
+#define UNLOADED_NODE ((void *)(1))
+#define INVALID_NODE_ID (-1)
+
+
+struct node_allocator
+{
+    struct node **nodes;
+    int64_t nodes_len;
+    int64_t nodes_alloc;
+    
+    int64_t loaded_nodes;
+
+    _Atomic int64_t access_token;
+
+    SRWLOCK lock;
+};
+
+
+
+
+
+
+struct node_allocator *allocator_create();
+void allocator_free(struct node_allocator *allocator);
+struct node *allocator_acquire_node_shared(struct node_allocator *allocator, int64_t node_id);
+struct node *allocator_release_node_shared(struct node_allocator *allocator, int64_t node_id);
+struct node *allocator_acquire_node_exclusive(struct node_allocator *allocator, int64_t node_id);
+struct node *allocator_release_node_exclusive(struct node_allocator *allocator, int64_t node_id);
+
+
+
+struct tree *tree_create();
+void tree_free(struct tree *tree);
+
+
+
+
+
+
+
+#endif
