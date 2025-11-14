c6c4bc4e82cf6bbe7913097e1b61a8eda2015922
sprogma <mux-228@yandex.ru>
2025-11-14 20:30:31 +0300

    add russian worker

--- build.ps1
+++ build.ps1
@@ -1,5 +1,6 @@
 $FLAGS = @("-g", "-fsanitize=address", "-D_CRT_SECURE_NO_WARNINGS", "-fms-extensions", "-Wno-microsoft")
 $x = @(gci *.h | % LastWriteTime)
-(gci *.c) + ($args|%{gi $_}) | ? {($x + @(gi $_ | % LastWriteTime)) -ge ((gi "$_.o" 2>$null) | % LastWriteTime)} | % { Write-Host "Building $($_.FullName)" -Foreground green ; clang $FLAGS -c ($_.FullName) -o "$($_.Name).o" }
+$f = (gci *.c) + ($args|%{gi $_})
+$f | ? {($x + @(gi $_ | % LastWriteTime)) -ge ((gi "$_.o" 2>$null) | % LastWriteTime)} | % { Write-Host "Building $($_.FullName)" -Foreground green ; clang $FLAGS -c ($_.FullName) -o "$($_.Name).o" }
 Write-Host "Linking..." -Foreground green
-clang $FLAGS (gci *.o) -o a.exe
+clang $FLAGS ($f | % Name | %{"$_.o"}) -o a.exe
